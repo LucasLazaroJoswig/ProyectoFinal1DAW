2ee717dca3b6e68f047cbdb3f8fb34d3c5ee29e0
DomenicoAlberico <albericodomenico@gmail.com>
2024-05-06 13:38:21 +0200

    primera versión del EmpleadoDao

--- src/modelo/dao/AbstractDaoImplmy8Jpa.java
+++ src/modelo/dao/AbstractDaoImplmy8Jpa.java
@@ -1,11 +1,12 @@
 package modelo.dao;
 
 import javax.persistence.EntityManager;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import com.mysql.cj.Query;
+import javax.persistence.Query;
 
 public abstract class AbstractDaoImplmy8Jpa {
 
